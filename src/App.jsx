f444b5f5513bb70dae1cf709a136e9660b6cd421
KiAnh2911 <matic2kaa@gmail.com>
2024-06-14 17:35:44 +0700

    add theme and modeToggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import DarkModeIcon from "@mui/icons-material/DarkMode";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import SettingsBrightnessIcon from "@mui/icons-material/SettingsBrightness";
+import { Box, Button } from "@mui/material";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import Select from "@mui/material/Select";
+import { useColorScheme } from "@mui/material/styles";
 
-function App() {
-  const [count, setCount] = useState(0)
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme();
 
+  const handleChange = (event) => {
+    setMode(event.target.value);
+  };
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <FormControl sx={{ m: 1, minWidth: 120 }} size="small">
+        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+        <Select
+          labelId="label-select-dark-light-mode"
+          id="select-dark-light-mode"
+          value={mode}
+          label="Mode"
+          onChange={handleChange}
+        >
+          <MenuItem value="light">
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <LightModeIcon fontSize="small" />
+              <span>Light</span>
+            </Box>
+          </MenuItem>
+          <MenuItem value="system">
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <SettingsBrightnessIcon fontSize="small" />
+              System
+            </Box>
+          </MenuItem>
+          <MenuItem value="dark">
+            <Box sx={{ display: "flex", alignItems: "center", gap: 1 }}>
+              <DarkModeIcon fontSize="small" />
+              Dark
+            </Box>
+          </MenuItem>
+        </Select>
+      </FormControl>
     </>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <>
+      <ModeToggle />
+      <Button>adsjasdasdjh</Button>
+    </>
+  );
+}
+
+export default App;
